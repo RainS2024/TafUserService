c5e57520d557ae8c2267a501c46a8a5861c83ab8
Garima S <garimas@tekarch.com>
2025-02-10 00:25:38 -0800

    added webconfig

---
+++ src/main/java/com/Tekarch/UserService/Config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.Tekarch.UserService.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Apply CORS settings to all endpoints
+                .allowedOrigins("http://localhost:4200")  // Allow your frontend's origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow all necessary methods
+                .allowedHeaders("*")  // Allow all headers
+                .allowCredentials(true);  // If you need cookies or other credentials
+    }
+}
